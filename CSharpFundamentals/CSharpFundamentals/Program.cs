e84e9039bec4b7fd67bce58c92f882702f31994a
Zack Hartmann <zackhartmann@gmail.com>
2017-11-21 13:13:51 -0500

    C#

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine(method.ToString());
+            Console.WriteLine((method == ShippingMethod.RegisteredAirMail)());
+            
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Variables
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            byte b = i;
+        }
+    }
+}
