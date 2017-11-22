f063ce0f16f96d5dd4394e09b39f17603d3984f6
Zack Hartmann <zackhartmann@gmail.com>
2017-11-21 19:39:11 -0500

    String Builder

---
+++ CSharpFundamentals66/CSharpFundamentals66/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals66
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+            
+            Console.WriteLine(builder);
+            
+            Console.WriteLine("First Char:" + builder[0]);
+        }
+    }
+}
