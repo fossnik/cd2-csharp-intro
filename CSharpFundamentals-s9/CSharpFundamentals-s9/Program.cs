d161be39340bc9ef53b6ba83d6801ed7e7b720a9
Zack Hartmann <zackhartmann@gmail.com>
2017-11-24 15:48:53 -0500

    Branch - Lecture 73

--- CSharpFundamentals-s9/CSharpFundamentals-s9/Program.cs
+++ CSharpFundamentals-s9/CSharpFundamentals-s9/Program.cs
@@ -8,35 +8,7 @@ namespace CSharpFundamentals_s9
     {
         public static void Main(string[] args)
         {
-            var path = @"/tmp/Providence-Rhode-Island.jpg";
-            
-            File.Copy( @"/tmp/Providence-Rhode-Island.jpg", @"/tmp/copy-Providence-Rhode-Island.jpg", true );
-            File.Delete( path );
-
-            if (File.Exists( path ))
-            {
-                Console.WriteLine( "{1} exists!", path );
-            }
-            else
-            {
-                Console.WriteLine( "{1} does not exist!", path );
-            }
-
-            var content = File.ReadAllText( path );
-            
-            var fileInfo = new FileInfo( path );
-            fileInfo.CopyTo( "/dev/null" );
-            
-            // takes no parameters
-            fileInfo.Delete();
-
-            if (fileInfo.Exists)
-            {
-                Console.WriteLine( @"fileInfo exists!
-note that fileInfo lacks the ReadAllText() method
-
-note the verbatim-string usage here" );
-            }
+            Directory.CreateDirectory(@"/tmp/folder1");
         }
     }
 }
