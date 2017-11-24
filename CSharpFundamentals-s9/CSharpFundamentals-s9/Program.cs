d5f5b4654ff40ba39e731646b2d275bc746f4d9a
Zack Hartmann <zackhartmann@gmail.com>
2017-11-24 16:43:52 -0500

    Branch - Lecture 74 - Directory Info

--- CSharpFundamentals-s9/CSharpFundamentals-s9/Program.cs
+++ CSharpFundamentals-s9/CSharpFundamentals-s9/Program.cs
@@ -9,6 +9,14 @@ namespace CSharpFundamentals_s9
         public static void Main(string[] args)
         {
             var path = "/home/seth/careerDevs-i/RiderProjects/CSharpFundamentals/HelloWorld/HelloWorld.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension     \t" + Path.GetExtension(path));
+            Console.WriteLine("File Name     \t" + Path.GetFileName(path));
+            Console.WriteLine("Sans Extension\t" + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name\t" + Path.GetDirectoryName(path));
         }
     }
 }
