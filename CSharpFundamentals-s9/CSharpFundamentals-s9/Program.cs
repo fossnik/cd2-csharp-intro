964fd57499a4ab1b626aeb7cd6e9e885c776332e
Zack Hartmann <zackhartmann@gmail.com>
2017-11-24 14:54:48 -0500

    S9 - Lecture 72

--- CSharpFundamentals-s9/CSharpFundamentals-s9/Program.cs
+++ CSharpFundamentals-s9/CSharpFundamentals-s9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace CSharpFundamentals_s9
 {
@@ -7,6 +8,36 @@ namespace CSharpFundamentals_s9
     {
         public static void Main(string[] args)
         {
+            var path = @"/tmp/Providence-Rhode-Island.jpg";
+            
+            File.Copy( @"/tmp/Providence-Rhode-Island.jpg", @"/tmp/copy-Providence-Rhode-Island.jpg", true );
+            File.Delete( path );
+
+            if (File.Exists( path ))
+            {
+                Console.WriteLine( "{1} exists!", path );
+            }
+            else
+            {
+                Console.WriteLine( "{1} does not exist!", path );
+            }
+
+            var content = File.ReadAllText( path );
+            
+            var fileInfo = new FileInfo( path );
+            fileInfo.CopyTo("/dev/null");
+            
+            // takes no parameters
+            fileInfo.Delete();
+
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine( @"fileInfo exists!
+note that fileInfo does not have a 'read all text' method
+
+note the verbatim-string usage here
+" );
+            }
         }
     }
 }
